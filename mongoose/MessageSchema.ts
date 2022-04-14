b9ca29496dc39f1800822b0dcb91aebf5efdbfab
Janvi Dankhara <janvidankhara@Janvis-MacBook-Pro.local>
2022-04-14 16:59:56 -0400

    Db and interfaces for conversation and message

--- mongoose/MessageSchema.ts
+++ mongoose/MessageSchema.ts
@@ -1,22 +1,22 @@
- /**
- * @file Implements mongoose schema for message
- */
-  import mongoose, {Schema} from "mongoose";
-  import Message from "../models/Message";
+/**
+* @file Implements mongoose schema for message
+*/
+import mongoose, {Schema} from "mongoose";
+import Message from "../models/Message";
 
-  /**
-  * @typedef Follow Represents user following another user,
-  * @property {string} message Text which needs to be sent
-  * @property {User} to User to whom messaged is to be sent
-  * @property {User} from User who is sending a message
-  * @property {Date} sentOn Date when message is sent
-  */
- 
-  const MessageSchema = new mongoose.Schema<Message>({
-      message: String,
-      to: {type: Schema.Types.ObjectId, ref: "UserModel"},
-      from: {type: Schema.Types.ObjectId, ref: "UserModel"},
-      sentOn: {type: Date, default: Date.now}
-  }, {collection: "messages"});
-  export default MessageSchema;
-  
+/**
+* @typedef Follow Represents user following another user,
+* @property {string} message Text which needs to be sent
+* @property {User} to User to whom messaged is to be sent
+* @property {User} from User who is sending a message
+* @property {Date} sentOn Date when message is sent
+*/
+
+const MessageSchema = new mongoose.Schema<Message>({
+  conversationId: String,
+  message: String,
+  to: {type: Schema.Types.ObjectId, ref: "UserModel"},
+  from: {type: Schema.Types.ObjectId, ref: "UserModel"},
+  sentOn: {type: Date, default: Date.now}
+}, {collection: "messages"});
+export default MessageSchema;
